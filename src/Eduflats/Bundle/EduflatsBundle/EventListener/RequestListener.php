f17305eb0720b97ca0ca4298c10e18eaaabac2db
unknown <mbilosha@gmail.com>
2015-09-14 17:10:15 +0530

    University Subdomain Logic

--- src/Eduflats/Bundle/EduflatsBundle/EventListener/RequestListener.php
+++ src/Eduflats/Bundle/EduflatsBundle/EventListener/RequestListener.php
@@ -2,21 +2,40 @@
 
 namespace Eduflats\Bundle\EduflatsBundle\EventListener;
 
+use Eduflats\Bundle\EduflatsBundle\Services\UniversityResolver;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Eduflats\Bundle\EduflatsBundle\Util;
+use Doctrine\ORM\EntityManager;
 
-class RequestListener
-{   
-    public function onKernelRequest(GetResponseEvent $event)
-    {
-        $subdomainList = Util::$subdomainList;
-        $UrlSubdomain = str_replace('.eduflats', '' , $event->getRequest()->getHost());
-        
-        if(!array_search($UrlSubdomain, $subdomainList)){
-            echo "susbdomain does not exists.";
-            exit;
-        }else{
-            Util::$currentId = array_search($UrlSubdomain, $subdomainList);
+class RequestListener {
+
+    protected $em;
+    protected $universityResolver;
+    protected $domain = array();
+
+    public function __construct(EntityManager $entityManager, UniversityResolver $universityResolver,$domain) {
+
+        $this->em = $entityManager;
+        $this->universityResolver = $universityResolver;
+        $this->domain = $domain;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event) {
+
+        //Get the domain name
+        $request = $event->getRequest();
+        //We do not need to handle it for the main website domain
+        if (in_array($request->getHttpHost(),$this->domain)) {
+            return;
         }
+
+        //Get the university Id
+        $universityId = $this->universityResolver->getUniversityId();
+        //If the university does not exist throw 404
+        if (!$universityId) {
+            throw new NotFoundHttpException('We do not currently have a website configured at this address. 
+                                  Please visit our website for more information or contact our support team');
+        }        
     }
 }
